8f53089acc3eb365013ea6a1def09e19ae45ca98
amurray2 <42621202+amurray2@users.noreply.github.com>
2021-04-10 22:16:59 -0600

    Backend Update

---
+++ Models/ViewModels/EditBurialViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FagElGamous.Models.ViewModels
+{
+    public class EditBurialViewModel
+    {
+        public IEnumerable<Location> Location { get; set; }
+        public Burial Burial { get; set; }
+    }
+}
