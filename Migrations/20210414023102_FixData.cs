7255503067edd37d68c75220e7b2843ad0276293
Curtis <curtmparker@gmail.com>
2021-04-13 21:00:40 -0600

    Fixed the Database

---
+++ Migrations/20210414023102_FixData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FagElGamous.Migrations
+{
+    public partial class FixData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "artifact_description",
+                table: "Artifacts",
+                unicode: false,
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1)",
+                oldUnicode: false,
+                oldMaxLength: 1,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "artifact_description",
+                table: "Artifacts",
+                type: "varchar(1)",
+                unicode: false,
+                maxLength: 1,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldUnicode: false,
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
